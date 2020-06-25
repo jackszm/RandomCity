0f95de63a24a39a322bbd5afc405270b4ca3533a
jackszm <jackszmelter@monzo.com>
2020-06-25 21:55:03 +0200

    Clean up Application class.

--- app/src/main/java/com/jsz/randomcity/RandomCityApp.kt
+++ app/src/main/java/com/jsz/randomcity/RandomCityApp.kt
@@ -2,12 +2,10 @@ package com.jsz.randomcity
 
 import android.annotation.SuppressLint
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.ProcessLifecycleOwner
 import androidx.room.Room
 import com.jsz.randomcity.db.AppDatabase
 import com.jsz.randomcity.db.DbCity
-import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 
 @SuppressLint("CheckResult")
@@ -20,31 +18,31 @@ class RandomCityApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        initDb()
+        observeProcessLifecycle()
+        storeProducerEvents()
+    }
+
+    private fun initDb() {
         db = Room
             .databaseBuilder(applicationContext, AppDatabase::class.java, "database")
             .build()
+    }
 
+    private fun observeProcessLifecycle() {
         ProcessLifecycleOwner
             .get()
             .lifecycle
             .addObserver(producer)
+    }
 
-        producer.events
+    private fun storeProducerEvents() {
+        producerEvents()
             .subscribeOn(Schedulers.io())
             .doOnNext {
-                db.cityStorage().insert(
-                    DbCity(
-                        name = it.city,
-                        color = it.color,
-                        timestamp = System.currentTimeMillis()
-                    )
-                )
+                db.cityStorage().insert(DbCity(it.city, it.color, System.currentTimeMillis()))
             }
-            .subscribeBy(
-                onNext = { Log.e("!!!", "Saving") }
-            )
-
-
+            .subscribe()
     }
 
     fun producerEvents() = producer.events
